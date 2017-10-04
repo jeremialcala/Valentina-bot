404d9caec6c4b1e32d9c16b8cef724c66cf872b4
Jeremi Meneses <jmeneses@novopayment.dom>
2017-10-04 18:36:41 -0400

    Fixing this up

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import sys
 import json
 from flask import Flask, request
-import telegram
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
@@ -24,6 +23,36 @@ def start(bot, update):
         "Hi! My name is Doctor Botter. I will hold a more complex conversation with you. "
         "Why don't you tell me something about yourself?",
         reply_markup=markup)
+
+    return CHOOSING
+
+
+def regular_choice(bot, update, user_data):
+    text = update.message.text
+    user_data['choice'] = text
+    update.message.reply_text('Your %s? Yes, I would love to hear about that!' % text.lower())
+
+    return TYPING_REPLY
+
+
+def custom_choice(bot, update):
+    update.message.reply_text('Alright, please send me the category first, '
+                              'for example "Most impressive skill"')
+
+    return TYPING_CHOICE
+
+
+def received_information(bot, update, user_data):
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
+
+    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
+                              "%s"
+                              "You can tell me more, or change your opinion on something."
+                              % facts_to_str(user_data),
+                              reply_markup=markup)
 
     return CHOOSING
 
@@ -61,7 +90,7 @@ def new_msg():
     headers = {
         "Content-Type": "application/json"
     }
-    
+
     TOKEN = "347715594:AAFxTVbmmV1pLhXAmnXLd72XWnxyYxqwlvE"
     updater = Updater(TOKEN)
 
@@ -71,6 +100,24 @@ def new_msg():
     log(data)
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
+        states={
+            CHOOSING: [RegexHandler('^(Age|Favourite colour|Number of siblings)$',
+                                    regular_choice,
+                                    pass_user_data=True),
+                       RegexHandler('^Something else...$',
+                                    custom_choice),
+                       ],
+
+            TYPING_CHOICE: [MessageHandler(Filters.text,
+                                           regular_choice,
+                                           pass_user_data=True),
+                            ],
+
+            TYPING_REPLY: [MessageHandler(Filters.text,
+                                          received_information,
+                                          pass_user_data=True),
+                           ],
+        },
         fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
     )
 
